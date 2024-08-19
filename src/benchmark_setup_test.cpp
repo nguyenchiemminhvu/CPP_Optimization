04f78f9fba3e064e567490965f51f9859da36bd9
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-08-19 14:33:37 +0000

    update

--- src/benchmark_setup_test.cpp
+++ src/benchmark_setup_test.cpp
@@ -1,40 +1,15 @@
 #include <benchmark/benchmark.h>
 
 #include <iostream>
-#include <set>
-#include <unordered_set>
 
-static void BM_test_set(benchmark::State& state)
+static void BM_test(benchmark::State& state)
 {
-    std::set<int> S;
-    for (int i = 0; i < 10; i++)
-    {
-        S.insert(i);
-    }
-
     for (auto _ : state)
     {
-        S.find(state.range(0));
+        benchmark::ClobberMemory();
     }
-    state.SetBytesProcessed(S.size() * state.iterations());
 }
 
-static void BM_test_unordered_set(benchmark::State& state)
-{
-    std::unordered_set<int> S;
-    for (int i = 0; i < 10; i++)
-    {
-        S.insert(i);
-    }
-
-    for (auto _ : state)
-    {
-        S.find(state.range(0));
-    }
-    state.SetBytesProcessed(S.size() * state.iterations());
-}
-
-BENCHMARK(BM_test_set)->Arg(5)->Arg(7);
-BENCHMARK(BM_test_unordered_set)->Arg(5)->Arg(7);
+BENCHMARK(BM_test);
 
 BENCHMARK_MAIN();
--- src/case_study_hot_statements.cpp
+++ src/case_study_hot_statements.cpp
@@ -3,11 +3,15 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <string_view>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 
 static void BM_uncache_loop_end_value(benchmark::State& state)
 {
-    char* input = new char[100000];
-    std::memset(input, ' ', 100000);
+    char* input = new char[100000U];
+    std::memset(input, ' ', 100000U);
     for (auto _ : state)
     {
         int count_whitespace = 0;
@@ -29,8 +33,8 @@ BENCHMARK(BM_uncache_loop_end_value);
 
 static void BM_cache_loop_end_value(benchmark::State& state)
 {
-    char* input = new char[100000];
-    std::memset(input, ' ', 100000);
+    char* input = new char[100000U];
+    std::memset(input, ' ', 100000U);
     std::size_t len = strlen(input);
     for (auto _ : state)
     {
@@ -51,52 +55,133 @@ BENCHMARK(BM_cache_loop_end_value);
 
 //////////////////////////////////////////////////////////////////
 
-void replace_whitespaces(char& c)
+static void BM_allocate_inside_loop(benchmark::State& state)
 {
-    if (c == ' ')
-    {
-        c = '_';
-    }
-}
-
-static void BM_function_call_overheat(benchmark::State& state)
-{
-    char* input = new char[100000];
-    std::memset(input, ' ', 100000);
-    std::size_t len = strlen(input);
     for (auto _ : state)
     {
-        for (std::size_t i = 0U; i < len; i++)
+        std::vector<int> data;
+        for (std::size_t i = 0U; i < 10000U; i++)
         {
-            replace_whitespaces(input[i]);
+            data.push_back(i);
         }
+        benchmark::DoNotOptimize(data);
         benchmark::ClobberMemory();
     }
-    delete[] input;
 }
-BENCHMARK(BM_function_call_overheat);
+BENCHMARK(BM_allocate_inside_loop);
 
 //////////////////////////////////////////////////////////////////
 
-static void BM_reduce_function_call_overheat(benchmark::State& state)
+static void BM_pre_allocate(benchmark::State& state)
 {
-    char* input = new char[100000];
-    std::memset(input, ' ', 100000);
-    std::size_t len = strlen(input);
     for (auto _ : state)
     {
-        for (std::size_t i = 0U; i < len; i++)
+        std::vector<int> data;
+        data.reserve(10000U);
+        for (std::size_t i = 0U; i < 10000U; i++)
         {
-            if (input[i] == ' ')
-            {
-                input[i] = '_';
-            }
+            data.push_back(i);
         }
+        benchmark::DoNotOptimize(data);
         benchmark::ClobberMemory();
     }
-    delete[] input;
 }
-BENCHMARK(BM_reduce_function_call_overheat);
+BENCHMARK(BM_pre_allocate);
+
+//////////////////////////////////////////////////////////////////
+
+static void BM_old_cpp_feature_string(benchmark::State& state)
+{
+    std::string s(10000U, ' ');
+    for (auto _ : state)
+    {
+        std::string temp = s;
+        benchmark::DoNotOptimize(temp);
+        benchmark::ClobberMemory();
+    }
+}
+BENCHMARK(BM_old_cpp_feature_string);
+
+//////////////////////////////////////////////////////////////////
+
+static void BM_modern_cpp_feature_string(benchmark::State& state)
+{
+    std::string s(10000U, ' ');
+    for (auto _ : state)
+    {
+        std::string_view temp = s;
+        benchmark::DoNotOptimize(temp);
+        benchmark::ClobberMemory();
+    }
+}
+BENCHMARK(BM_modern_cpp_feature_string);
+
+//////////////////////////////////////////////////////////////////
+
+static void BM_multi_branching(benchmark::State& state)
+{
+    std::string s(10000U, ' ');
+    for (auto _ : state)
+    {
+        int count = 0;
+        for (std::size_t i = 0U; i < s.length(); i++)
+        {
+            if (s[i] == ' ')
+            {
+                count++;
+            }
+        }
+        benchmark::DoNotOptimize(count);
+        benchmark::ClobberMemory();
+    }
+}
+BENCHMARK(BM_multi_branching);
+
+//////////////////////////////////////////////////////////////////
+
+static void BM_reduce_branching(benchmark::State& state)
+{
+    std::string s(10000U, ' ');
+    for (auto _ : state)
+    {
+        int count = 0;
+        for (char c : s)
+        {
+            count += (c == ' ');
+        }
+        benchmark::DoNotOptimize(count);
+        benchmark::ClobberMemory();
+    }
+}
+BENCHMARK(BM_reduce_branching);
+
+//////////////////////////////////////////////////////////////////
+
+static void BM_normal_copying(benchmark::State& state)
+{
+    std::vector<std::string> input(1000, "this is a string");
+    for (auto _ : state)
+    {
+        std::vector<std::string> res = input;
+        benchmark::DoNotOptimize(res);
+        benchmark::ClobberMemory();
+    }
+}
+BENCHMARK(BM_normal_copying);
+
+//////////////////////////////////////////////////////////////////
+
+static void BM_move_semantics(benchmark::State& state)
+{
+    std::vector<std::string> input(1000, "this is a string");
+    for (auto _ : state)
+    {
+        std::vector<std::string> res = std::move(input);
+        benchmark::DoNotOptimize(res);
+        benchmark::ClobberMemory();
+    }
+}
+BENCHMARK(BM_move_semantics);
 
 
 BENCHMARK_MAIN();
