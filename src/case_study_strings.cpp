e66c348d66e3d2e0bf120ff41313550eb9a29b94
vu3.nguyen <vu3.nguyen@lge.com>
2024-08-19 13:20:24 +0900

    string naive case

--- src/case_study_strings.cpp
+++ src/case_study_strings.cpp
@@ -1,14 +1,31 @@
 #include <benchmark/benchmark.h>
 #include <string>
 
+std::string trim_whitespace_naive(std::string s)
+{
+    std::string res;
+    for (std::size_t i = 0U; i < s.length(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            res = res + s[i];
+        }
+    }
+
+    return res;
+}
+
 static void BM_trim_whitespace_naive(benchmark::State& state)
 {
+    std::string input = "this is a string";
     for (auto _ : state)
     {
-
+        std::string res = trim_whitespace_naive(input);
     }
 }
 BENCHMARK(BM_trim_whitespace_naive);
+
+/////////////////////////////////////////////////////////////////////////////////
 
 static void BM_trim_whitespace_eliminate_temporaries(benchmark::State& state)
 {
@@ -19,6 +36,8 @@ static void BM_trim_whitespace_eliminate_temporaries(benchmark::State& state)
 }
 BENCHMARK(BM_trim_whitespace_eliminate_temporaries);
 
+/////////////////////////////////////////////////////////////////////////////////
+
 static void BM_trim_whitespace_reserve_storage(benchmark::State& state)
 {
     for (auto _ : state)
@@ -27,6 +46,8 @@ static void BM_trim_whitespace_reserve_storage(benchmark::State& state)
     }
 }
 BENCHMARK(BM_trim_whitespace_reserve_storage);
+
+/////////////////////////////////////////////////////////////////////////////////
 
 static void BM_trim_whitespace_eliminate_copying_args(benchmark::State& state)
 {
@@ -37,6 +58,8 @@ static void BM_trim_whitespace_eliminate_copying_args(benchmark::State& state)
 }
 BENCHMARK(BM_trim_whitespace_eliminate_copying_args);
 
+/////////////////////////////////////////////////////////////////////////////////
+
 static void BM_trim_whitespace_iterators(benchmark::State& state)
 {
     for (auto _ : state)
@@ -45,6 +68,8 @@ static void BM_trim_whitespace_iterators(benchmark::State& state)
     }
 }
 BENCHMARK(BM_trim_whitespace_iterators);
+
+/////////////////////////////////////////////////////////////////////////////////
 
 static void BM_trim_whitespace_eliminate_copying_returned(benchmark::State& state)
 {
@@ -55,6 +80,8 @@ static void BM_trim_whitespace_eliminate_copying_returned(benchmark::State& stat
 }
 BENCHMARK(BM_trim_whitespace_eliminate_copying_returned);
 
+/////////////////////////////////////////////////////////////////////////////////
+
 static void BM_trim_whitespace_c_strings(benchmark::State& state)
 {
     for (auto _ : state)
@@ -64,6 +91,8 @@ static void BM_trim_whitespace_c_strings(benchmark::State& state)
 }
 BENCHMARK(BM_trim_whitespace_c_strings);
 
+/////////////////////////////////////////////////////////////////////////////////
+
 static void BM_trim_whitespace_improved_algorithm(benchmark::State& state)
 {
     for (auto _ : state)
@@ -72,6 +101,8 @@ static void BM_trim_whitespace_improved_algorithm(benchmark::State& state)
     }
 }
 BENCHMARK(BM_trim_whitespace_improved_algorithm);
+
+/////////////////////////////////////////////////////////////////////////////////
 
 static void BM_trim_whitespace_improved_allocator(benchmark::State& state)
 {
