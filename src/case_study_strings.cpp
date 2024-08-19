dd6e2b080caf7d3b3e8005a2b796be3342bb1838
vu3.nguyen <vu3.nguyen@lge.com>
2024-08-19 13:12:34 +0900

    prepare for case study string

--- src/case_study_strings.cpp
+++ src/case_study_strings.cpp
@@ -1,14 +1,86 @@
 #include <benchmark/benchmark.h>
 #include <string>
 
-static void BM_remove_ctrl_naive(benchmark::State& state)
+static void BM_trim_whitespace_naive(benchmark::State& state)
 {
     for (auto _ : state)
     {
 
     }
 }
+BENCHMARK(BM_trim_whitespace_naive);
 
-BENCHMARK(BM_remove_ctrl_naive);
+static void BM_trim_whitespace_eliminate_temporaries(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+
+    }
+}
+BENCHMARK(BM_trim_whitespace_eliminate_temporaries);
+
+static void BM_trim_whitespace_reserve_storage(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+
+    }
+}
+BENCHMARK(BM_trim_whitespace_reserve_storage);
+
+static void BM_trim_whitespace_eliminate_copying_args(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+
+    }
+}
+BENCHMARK(BM_trim_whitespace_eliminate_copying_args);
+
+static void BM_trim_whitespace_iterators(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+
+    }
+}
+BENCHMARK(BM_trim_whitespace_iterators);
+
+static void BM_trim_whitespace_eliminate_copying_returned(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+
+    }
+}
+BENCHMARK(BM_trim_whitespace_eliminate_copying_returned);
+
+static void BM_trim_whitespace_c_strings(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+
+    }
+}
+BENCHMARK(BM_trim_whitespace_c_strings);
+
+static void BM_trim_whitespace_improved_algorithm(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+
+    }
+}
+BENCHMARK(BM_trim_whitespace_improved_algorithm);
+
+static void BM_trim_whitespace_improved_allocator(benchmark::State& state)
+{
+    for (auto _ : state)
+    {
+
+    }
+}
+BENCHMARK(BM_trim_whitespace_improved_allocator);
+
 
 BENCHMARK_MAIN();
