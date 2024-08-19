625307309e942bb7998df80ec8fc19f47c8db1c4
vu3.nguyen <vu3.nguyen@lge.com>
2024-08-19 14:17:09 +0900

    some efforts to improve string

--- src/case_study_strings.cpp
+++ src/case_study_strings.cpp
@@ -27,55 +27,103 @@ BENCHMARK(BM_trim_whitespace_naive);
 
 /////////////////////////////////////////////////////////////////////////////////
 
+std::string trim_whitespace_eliminate_temporaries(std::string s)
+{
+    std::string res;
+    for (std::size_t i = 0U; i < s.length(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            res += s[i];
+        }
+    }
+
+    return res;
+}
+
 static void BM_trim_whitespace_eliminate_temporaries(benchmark::State& state)
 {
+    std::string input = "this is a string";
     for (auto _ : state)
     {
-
+        std::string res = trim_whitespace_eliminate_temporaries(input);
     }
 }
 BENCHMARK(BM_trim_whitespace_eliminate_temporaries);
 
 /////////////////////////////////////////////////////////////////////////////////
 
-static void BM_trim_whitespace_reserve_storage(benchmark::State& state)
+std::string trim_whitespace_iterators(std::string s)
 {
-    for (auto _ : state)
+    std::string res;
+    for (auto it = s.begin(); it != s.end(); it++)
     {
-
+        if (*it != ' ')
+        {
+            res += *it;
+        }
     }
+
+    return res;
 }
-BENCHMARK(BM_trim_whitespace_reserve_storage);
-
-/////////////////////////////////////////////////////////////////////////////////
-
-static void BM_trim_whitespace_eliminate_copying_args(benchmark::State& state)
-{
-    for (auto _ : state)
-    {
-
-    }
-}
-BENCHMARK(BM_trim_whitespace_eliminate_copying_args);
-
-/////////////////////////////////////////////////////////////////////////////////
 
 static void BM_trim_whitespace_iterators(benchmark::State& state)
 {
+    std::string input = "this is a string";
     for (auto _ : state)
     {
-
+        std::string res = trim_whitespace_iterators(input);
     }
 }
 BENCHMARK(BM_trim_whitespace_iterators);
 
 /////////////////////////////////////////////////////////////////////////////////
 
-static void BM_trim_whitespace_eliminate_copying_returned(benchmark::State& state)
+std::string trim_whitespace_eliminate_copying_args(const std::string& s)
 {
+    std::string res;
+    for (std::size_t i = 0U; i < s.length(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            res += s[i];
+        }
+    }
+
+    return res;
+}
+
+static void BM_trim_whitespace_eliminate_copying_args(benchmark::State& state)
+{
+    std::string input = "this is a string";
     for (auto _ : state)
     {
+        std::string res = trim_whitespace_eliminate_copying_args(input);
+    }
+}
+BENCHMARK(BM_trim_whitespace_eliminate_copying_args);
 
+/////////////////////////////////////////////////////////////////////////////////
+
+void trim_whitespace_eliminate_copying_returned(const std::string& s, std::string& res)
+{
+    res.clear();
+    for (std::size_t i = 0U; i < s.length(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            res += s[i];
+        }
+    }
+}
+
+static void BM_trim_whitespace_eliminate_copying_returned(benchmark::State& state)
+{
+    std::string input = "this is a string";
+    std::string res;
+    for (auto _ : state)
+    {
+        trim_whitespace_eliminate_copying_returned(input, res);
     }
 }
 BENCHMARK(BM_trim_whitespace_eliminate_copying_returned);
